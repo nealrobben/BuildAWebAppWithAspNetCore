cb09869b2ffea9d21c5e9115d7b9b45b6ca0bf4e
Neal Robben <neal.robben@cegeka.com>
2019-07-19 17:16:24 +0200

    Added entity framework core

---
+++ DutchTreat/DutchTreat/Data/DutchContext.cs
@@ -0,0 +1,11 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -1,6 +1,8 @@
-﻿using DutchTreat.Services;
+﻿using DutchTreat.Data;
+using DutchTreat.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
@@ -11,7 +13,13 @@ namespace DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DutchContext>(cfg =>
+            {
+                cfg.UseSqlServer("");
+            });
+
             services.AddTransient<IMail, NullMailService>();
+
             services.AddMvc();
         }
 
