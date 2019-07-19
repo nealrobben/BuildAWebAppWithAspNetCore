f731f4b8bec57b6fd311b1047e52fb4776612d9a
Neal Robben <neal.robben@cegeka.com>
2019-07-19 14:46:45 +0200

    include JQuery & Bootstrap

--- DutchTreat/DutchTreat/Startup.cs
+++ DutchTreat/DutchTreat/Startup.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DutchTreat
@@ -18,6 +17,7 @@ namespace DutchTreat
         {
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseNodeModules(env);
         }
     }
 }
