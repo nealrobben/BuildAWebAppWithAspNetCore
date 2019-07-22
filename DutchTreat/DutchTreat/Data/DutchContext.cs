89810b595395c675345d5fd777ee9ee8d773cbcc
Neal Robben <neal.robben@cegeka.com>
2019-07-22 15:32:14 +0200

    Add seeded data

--- DutchTreat/DutchTreat/Data/DutchContext.cs
+++ DutchTreat/DutchTreat/Data/DutchContext.cs
@@ -1,4 +1,5 @@
-﻿using DutchTreat.Data.Entities;
+﻿using System;
+using DutchTreat.Data.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace DutchTreat.Data
@@ -11,5 +12,18 @@ namespace DutchTreat.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Order>().HasData(
+                new Order
+                {
+                    Id = 1,
+                    OrderDate = DateTime.UtcNow,
+                    OrderNumber = "12345"
+                });
+        }
     }
 }
