002d00dcfa7fa49bd19477932171f69aecc22d8f
Neal Robben <neal.robben@cegeka.com>
2019-07-24 15:49:17 +0200

    Share data accross components

--- DutchTreat/DutchTreat/ClientApp/app/shared/dataService.ts
+++ DutchTreat/DutchTreat/ClientApp/app/shared/dataService.ts
@@ -4,6 +4,8 @@ import { Injectable } from "@angular/core";
 import { map } from "rxjs/operators";
 import { Observable } from "rxjs"
 import "rxjs/add/operator/map";
+import { Product } from "./product";
+import { Order, OrderItem } from "./order";
 
 @Injectable()
 export class DataService {
@@ -11,14 +13,34 @@ export class DataService {
     constructor(private http: HttpClient) {
     }
 
-    public products = [];
+    public order: Order = new Order();
+
+    public products: Product[] = [];
 
     loadProducts(): Observable<boolean> {
-        this.http.get("/api/products")
+        return this.http.get("/api/products")
             .map((data: any[]) => {
                 this.products = data;
                 return true;
             });
+    }
+
+    public addToOrder(newProduct: Product) {
+
+        var item: OrderItem = new OrderItem();
+
+        item.productId = newProduct.id;
+
+        item.productId = newProduct.id;
+        item.productArtist = newProduct.artist;
+        item.productArtId = newProduct.artId;
+        item.productCategory = newProduct.category;
+        item.productSize = newProduct.size;
+        item.productTitle = newProduct.title;
+        item.unitPrice = newProduct.price;
+        item.quantity = 1;
+
+        this.order.items.push(item);
     }
 
 }
--- DutchTreat/DutchTreat/ClientApp/app/shop/productList.component.ts
+++ DutchTreat/DutchTreat/ClientApp/app/shop/productList.component.ts
@@ -1,7 +1,7 @@
 ﻿import { Component } from "@angular/core";
 import { DataService } from "../shared/dataService";
 import { Observable } from "rxjs";
-import { Product } from "../shared/product";
+import { Product, Product as IProduct } from "../shared/product";
 
 
 @Component({
@@ -10,18 +10,18 @@ import { Product } from "../shared/product";
     styleUrls: ["productList.component.css"]
 })
 export class ProductList implements OnInit {
+    public products: Product[];
 
     constructor(private data: DataService) {
         this.products = data.products;
     }
 
-    public products: Product[] = [];
+    ngOnInit() {
+        this.data.loadProducts()
+            .subscribe(() => this.products = this.data.products);
+    }
 
-    ngOnInit(): void {
-        this.data.loadProducts().subscribe(success => {
-            if (success) {
-                this.products = this.data.products;
-            }
-        });
+    addProduct(product: Product) {
+        this.data.addToOrder(product);
     }
 }
